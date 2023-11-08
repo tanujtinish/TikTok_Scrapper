7fe7d798471b593ac2c73bae50f95fe81de843f7
Tanuj Gupta <tanujgupta@Tanujs-MacBook-Pro.local>
2023-11-07 20:39:17 -0600

    updated main job to fetch posts

--- src/jobs/fetch_fasion_posts.py
+++ src/jobs/fetch_fasion_posts.py
@@ -4,21 +4,50 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 from src.services.tiktok_recommendation_browser_session import TiktTokRecommendationBrowserSession
 
-from src.database.models.posts import Posts
+from src.database.models.post import Post
 
 import asyncio
 
-# Example usage:
-ms_token = "K0A_4yeeT2o4VZRofZzNhSGGjrstUFiE6FCrG9jtOWTKP_XtPFuCadkKj7yxbUNNNbNtPidJtBx62VwudNXJRHA_TEp5ZTxOZgi0jzy7Tzvv1WOjNR3CnhiPHDROJFcROQ5UT8WaRnPmb9cP"
-session = TiktTokRecommendationBrowserSession(ms_token)
-posts = Posts(session)
-asyncio.run(posts.parse_response())
+
+async def fetch_fasion_trending_posts(count):
+    # Example usage:
+    ms_token = "K0A_4yeeT2o4VZRofZzNhSGGjrstUFiE6FCrG9jtOWTKP_XtPFuCadkKj7yxbUNNNbNtPidJtBx62VwudNXJRHA_TEp5ZTxOZgi0jzy7Tzvv1WOjNR3CnhiPHDROJFcROQ5UT8WaRnPmb9cP"
+    browser_session = TiktTokRecommendationBrowserSession(ms_token)
+
+    # Make a request using the session
+    api_url = "https://www.tiktok.com/api/recommend/item_list/"
+    posts_response_data = browser_session.make_request(api_url, {"count":count})
+
+    if "itemList" in posts_response_data:
+        itemList = posts_response_data["itemList"]
+    else:
+        itemList = []
 
 
-print(len(posts.posts))
-if(len(posts.posts)>0):
-    print(posts.posts[2].hashtags.titles)
-    # print(posts.posts[0].comments.titles)
+    if(len(itemList) > 0):
+        post_obj = Post(itemList[0], browser_session)
+        url = f"https://www.tiktok.com/@{post_obj.author.unique_id}/video/{post_obj.video_id}"
+        # url = "https://google.com"
+        browser_session.solve_captcha_for_other_sessions(url)
+        
+        tasks = []
+        for item in itemList:
+            post_obj = Post(item, browser_session)
+            task = asyncio.create_task(post_obj.fetchPostWithComments())
+            tasks.append(task)
+        
+        results = await asyncio.gather(*tasks)
+        return results
+    else:
+        return []
+
+
+posts = asyncio.run(fetch_fasion_trending_posts(30))
+
+print(len(posts))
+if(len(posts)>0):
+    print(posts[2].hashtags.titles)
+    # print(posts[0].comments.titles)
 
 # Close the session when done
 # session.close_session()
