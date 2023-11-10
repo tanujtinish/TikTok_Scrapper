b8182778b9408efb8b05e4d6aa51a00591f18115
Tanuj Gupta <tanujgupta@Tanujs-MacBook-Pro.local>
2023-11-09 20:08:48 -0600

    folder to store apis from twitter

---
+++ src/twitter_apis/twitter_web_apis.py
@@ -0,0 +1,4 @@
+
+tiktok_posts_recommendations_api= "https://www.tiktok.com/api/recommend/item_list/"
+
+tiktok_posts_fasion_api = "https://www.tiktok.com/browse/api/get-video-list?label=10029&region=US"
