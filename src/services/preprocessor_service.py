507a7dc08e726163a97f45e68da2bcc46e967085
Tanuj Gupta <tanujgupta@Tanujs-MacBook-Pro.local>
2023-11-10 07:23:04 -0600

    added functions in preprocessor to get all english words

--- src/services/preprocessor_service.py
+++ src/services/preprocessor_service.py
@@ -1,48 +1,48 @@
 import nltk
 import re
 import string
+
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 from nltk.corpus import wordnet
 
-nltk.download('punkt')
-nltk.download('wordnet')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('stopwords')
-
 # https://medium.com/analytics-vidhya/nlp-tutorial-for-text-classification-in-python-8f19cd17b49e
 
 # Convert to lowercase, strip, and remove punctuations
 def basic_preprocess(text):
-    # Convert to lowercase
+    
     text = text.lower()
-    # Strip leading and trailing whitespaces
+    
     text = text.strip()
-    # Remove HTML tags
+    
     text = re.compile('<.*?>').sub('', text)
-    # Remove punctuation
+    
     text = re.compile('[%s]' % re.escape(string.punctuation)).sub(' ', text)
-    # Remove multiple spaces
+    
     text = re.sub('\s+', ' ', text)
-    # Remove digits
+    
     text = re.sub(r'\d', ' ', text)
-    # Remove extra whitespaces
+    
     text = re.sub(r'\s+', ' ', text)
     return text
 
-# STOPWORD REMOVAL
 def stopword(string):
     stop_words = set(stopwords.words('english'))
     words = word_tokenize(string)
     filtered_words = [word for word in words if word.lower() not in stop_words]
     return ' '.join(filtered_words)
 
-# LEMMATIZATION
-# Initialize the lemmatizer
-wl = WordNetLemmatizer()
+def remove_smaller_words(words, word_len):
+    filtered_words = [word for word in words if (len(word) > word_len)]
+    return ' '.join(filtered_words)
+    
+def remove_non_english_words(string):
+    english_words = set(word.lower() for word in nltk.corpus.words.words())
+    words = word_tokenize(string)
+    filtered_words = [word for word in words if (word.lower() in english_words and len(word) > 4)]
+    return ' '.join(filtered_words)
 
-# This is a helper function to map NTLK position tags
 def get_wordnet_pos(tag):
     if tag.startswith('J'):
         return wordnet.ADJ
@@ -55,15 +55,21 @@ def get_wordnet_pos(tag):
     else:
         return wordnet.NOUN
 
-# Tokenize the sentence
 def lemmatizer(string):
-    word_pos_tags = nltk.pos_tag(word_tokenize(string)) # Get position tags
-    lemmatized_words = [wl.lemmatize(tag[0], get_wordnet_pos(tag[1])) for tag in word_pos_tags] # Map the position tag and lemmatize the word/token
+    word_pos_tags = nltk.pos_tag(word_tokenize(string))
+    
+    wl = WordNetLemmatizer()
+    lemmatized_words = [wl.lemmatize(tag[0], get_wordnet_pos(tag[1])) for tag in word_pos_tags]
     return " ".join(lemmatized_words)
 
-# Final preprocessing combining all steps
 def preprocess(string):
     preprocessed_text = basic_preprocess(string)
     without_stopwords = stopword(preprocessed_text)
+    without_small_and_stop_words = remove_smaller_words(without_stopwords, 3)
     lemmatized_text = lemmatizer(without_stopwords)
     return lemmatized_text
+
+# text = "For writers, a random sentence can help them get their creative juices flowing. Since the topic of the sentence is completely unknown, it forces the writer to be creative when the sentence appears. There are a number of different ways a writer can use the random sentence for creativity. The most common way to use the sentence is to begin a story. Another option is to include it somewhere in the story. A much more difficult challenge is to use it to end a story. In any of these cases, it forces the writer to think creatively since they have no idea what sentence will appear from the tool."
+# print(text)
+# text = preprocess(text)
+# print(text)
