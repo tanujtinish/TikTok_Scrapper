51caa52e652375dcd4b35d7b6a9afcfab074a66e
Tanuj Gupta <tanujgupta@Tanujs-MacBook-Pro.local>
2023-11-09 20:07:20 -0600

    updated database models constructors to accomodate response from 2 apis

---
+++ src/database/models/hashtag.py
@@ -0,0 +1,18 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+
+from src.services.preprocessor_service import preprocess
+
+class Hashtag:
+    def __init__(self, title, desc):
+        self.title = preprocess(title)
+        self.desc = preprocess(desc)
+
+    def to_dict(self):
+        # Convert the Hashtags object to a dictionary
+        hashtags_dict = {
+            "title": self.title,
+            "desc": self.desc,
+        }
+        return hashtags_dict
