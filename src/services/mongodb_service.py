d9a6330626d7074ee577b559dcc86b92cf21f9c5
Tanuj Gupta <tanujgupta@Tanujs-MacBook-Pro.local>
2023-11-09 20:17:31 -0600

    added device pi before calling tiktok api

--- src/services/mongodb_service.py
+++ src/services/mongodb_service.py
@@ -16,22 +16,9 @@ class Mongodb_service:
   def save_many_to_mongodb(self, data):
     self.collection.insert_many(data)
 
-  def get_all_crawed_data(self):
-    res = self.collection.find({"status": "C"})
-    return res
-  
-  def update_status_batch(self, ids):
-    self.collection.update_many({"_id": {"$in": ids}}, {"$set": {"status": "T"}})
-    print("Updated status to T")
+  def update_batch(self, ids):
+    self.collection.update_many({"_id": {"$in": ids}}, {"$set": {"": ""}})
 
   def find_by_id(self, id):
     res = self.collection.find_one({"_id": id})
     return res
-
-  def get_all_uncrawled_websites(self):
-    res = self.collection.find({"crawled": "Pending"}).limit(10)
-    return res
-  
-  def set_crawled_website(self, url):
-    self.collection.update_one({"website_url": url}, {"$set": {"crawled": "Crawled"}})
-    print("Updated status to Crawled")
