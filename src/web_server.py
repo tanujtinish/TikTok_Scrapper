1274285f4713ebdc7d4e13503865133a9ef4f7bb
Tanuj Gupta <tanujgupta@Tanujs-MacBook-Air.local>
2023-11-11 03:10:31 -0600

    updated apis to post where data updated

--- src/web_server.py
+++ src/web_server.py
@@ -16,9 +16,10 @@ CORS(app)
 def health_check():
     return jsonify({"success": True}), 200
 
-@app.route('/scrape_tiktok_posts')
+@app.route('/scrape_tiktok_posts', methods=['PUT'])
 def scrape_tiktok_posts():
     try:
+        print("enter api scrape_tiktok_posts")
         count = int(request.args.get('posts_to_scrape', 30))
         cursor = int(request.args.get('cursor', 0))
 
@@ -28,81 +29,103 @@ def scrape_tiktok_posts():
         for post in posts:
             post["date_collected"] = str(post["date_collected"])
             post["_id"] = str(post["_id"])
+        
+        print("exit api scrape_tiktok_posts: " + str(len(posts)))
         return jsonify({"success": True, "posts": posts}), 200
     except Exception as e:
+        print("error api scrape_tiktok_posts: "+str(e))
         return jsonify({"success": False, "error": str(e)}), 500
 
 
-@app.route('/fetch_comments_for_scrapped_posts')
+@app.route('/fetch_comments_for_scrapped_posts', methods=['PUT'])
 async def fetch_comments_for_scrapped_posts():
     try:
+        print("enter api fetch_comments_for_scrapped_posts")
         posts = await fetch_comments_for_posts_controller()
 
         for post in posts:
             post["date_collected"] = str(post["date_collected"])
             post["_id"] = str(post["_id"])
+
+        print("exit api fetch_comments_for_scrapped_posts: " + str(len(posts)))
         return jsonify({"success": True, "posts_with_comments": posts}), 200
     except Exception as e:
+        print("error api fetch_comments_for_scrapped_posts: "+str(e))
         return jsonify({"success": False, "error": str(e)}), 500
 
 
-@app.route('/filter_fasion_posts_with_relevance_scores')
+@app.route('/filter_fasion_posts_with_relevance_scores', methods=['PUT'])
 def filter_fasion_posts_with_relevance_scores():
     try:
+        print("enter api filter_fasion_posts_with_relevance_scores")
         posts = assign_relevance_scores_and_filter_fasion_posts()
 
         for post in posts:
             post["date_collected"] = str(post["date_collected"])
             post["_id"] = str(post["_id"])
+
+        print("exit api filter_fasion_posts_with_relevance_scores: " + str(len(posts)))
         return jsonify({"success": True, "posts": posts}), 200
     except Exception as e:
+        print("error api filter_fasion_posts_with_relevance_scores: "+str(e))
         return jsonify({"success": False, "error": str(e)}), 500
 
 
-@app.route('/process_instagram_post_data')
+@app.route('/process_instagram_post_data', methods=['PUT'])
 def process_instagram_post_data():
     try:
+        print("enter api process_instagram_post_data")
         num_topics = int(request.args.get('num_topics', 10))
         words_per_topic = int(request.args.get('words_per_topic', 20))
         
         posts = process_fasion_instagram_data(num_topics, words_per_topic)
 
+        print("exit api process_instagram_post_data: " + str(len(posts)))
         return jsonify({"success": True, "posts": posts}), 200
     except Exception as e:
+        print("error api process_instagram_post_data: "+str(e))
         return jsonify({"success": False, "error": str(e)}), 500
 
 
 @app.route('/get_scraped_fasion_posts')
 def process_instagram_post_data():
     try:
+        print("enter api get_scraped_fasion_posts")
         mongodb_service_source = Mongodb_service(fasion_posts_collection)
         post_objs_mongo = mongodb_service_source.find()
 
+        print("exit api get_scraped_fasion_posts: " + str(len(post_objs_mongo)))
         return jsonify({"success": True, "scraped_fasion_posts": post_objs_mongo}), 200
     except Exception as e:
+        print("error api get_scraped_fasion_posts: "+str(e))
         return jsonify({"success": False, "error": str(e)}), 500
 
 
 @app.route('/get_scraped_posts')
 def process_instagram_post_data():
     try:
+        print("enter api scrape_tiktok_posts")
         mongodb_service_source = Mongodb_service(scraped_posts_collection)
         post_objs_mongo = mongodb_service_source.find()
 
+        print("exit api scrape_tiktok_posts: " + str(len(post_objs_mongo)))
         return jsonify({"success": True, "scraped_posts": post_objs_mongo}), 200
     except Exception as e:
+        print("error api scrape_tiktok_posts: "+str(e))
         return jsonify({"success": False, "error": str(e)}), 500
 
 
 @app.route('/get_scraped_posts_with_comments')
 def process_instagram_post_data():
     try:
-        
+        print("enter api get_scraped_posts_with_comments")
         mongodb_service_source = Mongodb_service(scraped_posts_with_comments_collection)
         post_objs_mongo = mongodb_service_source.find()
 
+        print("exit api get_scraped_posts_with_comments: " + str(len(post_objs_mongo)))
         return jsonify({"success": True, "scraped_posts_with_comments": post_objs_mongo}), 200
     except Exception as e:
+        print("error api get_scraped_posts_with_comments: "+str(e))
         return jsonify({"success": False, "error": str(e)}), 500
 
 
