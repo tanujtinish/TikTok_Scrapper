d1603d7eb290616edd7c92bcf72388bd013fca34
tanujtinish <tanujtinish@gmail.com>
2023-11-11 05:44:35 -0600

    logging

--- src/configs/mongodb_config.py
+++ src/configs/mongodb_config.py
@@ -7,10 +7,10 @@ def connect_to_mongodb():
     try:
         client = pymongo.MongoClient(connection_string)
         db = client[database_name]
-        app.logger.info("Connected to MongoDB")
+        print("Connected to MongoDB")
         return db
     except Exception as e:
-        app.logger.info("Error connecting to MongoDB: ", e)
+        print("Error connecting to MongoDB: ", e)
         return None
     
 client = connect_to_mongodb()
